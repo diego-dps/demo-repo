106bed94d511e6b70a1383bc58c8df8b4b03f389
diego-dps <diego.pereira2806@hotmail.com>
2023-08-29 17:08:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Box } from "@chakra-ui/react";
+import Demonstracao from "./components/Demonstracao";
+
+function App() {
+  return (
+    <>
+      <Box>
+        <Demonstracao />
+      </Box>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Demonstracao/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+// import { Container } from './styles';
+
+function Demonstracao() {
+  return(
+    <Box >
+        <Box>Demonstração</Box>
+    </Box>
+  )
+}
+
+export default Demonstracao;
